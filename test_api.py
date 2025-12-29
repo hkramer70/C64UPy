c5206eac6712e48caa79179f03e1f7d72a3c14a8
Birko <birko70@mailbox.org>
2025-12-29 15:00:27 +0100

    Added test

--- test_api.py
+++ test_api.py
@@ -1,5 +1,6 @@
 import pytest
 from C64UApi import Api, ApiMethod
+import C64UModel
 
 def test_api_version_value():
     """Test that Api.VERSION has the correct value."""
@@ -21,3 +22,16 @@ def test_api_rest_path():
     """Test the restPath method."""
     path = Api.VERSION.restPath()
     assert "http://c64u/v1/version" == path  # Based on default config
+
+def test_api_rest_path_mount_img():
+    """Test the restPath method."""
+    path = Api.MOUNT_IMG
+    pathDefault = path.restPath()
+    pathA = path.restPath(C64UModel.Drive.A)
+    pathB = Api.MOUNT_IMG.restPath(C64UModel.Drive.B)
+    pathSoft = Api.MOUNT_IMG.restPath(C64UModel.Drive.SOFTIEC)
+    assert "http://c64u/v1/drives/a:mount" == pathDefault  # Based on default config
+    assert "http://c64u/v1/drives/a:mount" == pathA  # Based on default config
+    assert "http://c64u/v1/drives/b:mount" == pathB  # Based on default config
+    assert "http://c64u/v1/drives/softiec:mount" == pathSoft
+
