29ef9ef973b14ef5714a296b13bc8c3237e4162f
Birko <birko70@mailbox.org>
2025-12-29 01:03:44 +0100

    Changed app arguments

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-from C64UClient import C64UClient
+from C64UClient import Client
 
 #
 # Main entry point for the C64 Ultimate PRG uploader.
@@ -10,21 +10,34 @@ from C64UClient import C64UClient
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Sendet ein PRG an den C64 Ultimate und startet es"
+        description="Kommuniziert mit dem Commodore64 Ultimate",
+        add_help=False
     )
 
     parser.add_argument(
-        "filename",
-        help="Pfad zur PRG-Datei"
+        "-h", "--h",
+        action="help",
+        help="Zeige diese Hilfe an"
+    )
+
+    parser.add_argument(
+        "-runprg",
+        metavar="<Pfad zum PRG File>",
+        help="Überträgt zum und startet die PRG-Datei auf dem C64U"
     )
 
     args = parser.parse_args()
-    fname = args.filename
+    client = Client()
 
-    client = C64UClient()
-    success = client.runPrg(fname)
-
-    print("Done" if success else "Error")
+    if args.runprg:
+        success = client.runPrg(args.runprg)
+        print("Done" if success else "Error")
+    else:
+        version = client.requestVersion()
+        if version:
+            print(f"REST-API Version: {version.version}")
+        else:
+            print("Error getting information from C64U")
 
 if __name__ == "__main__":
     main()
